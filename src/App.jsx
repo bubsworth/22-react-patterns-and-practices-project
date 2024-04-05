ff440aaf31da1258a761149b1f46c081cb407e67
bubsworth <bubbard96@hotmail.com>
2024-04-05 18:19:51 +0100

    Managing Multi-Component State with the Context APIs

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,31 @@ import AccordionItem from "./components/Accordion/AccordionItem";
 function App() {
   return (  
     <main>
-      <seciton>
+      <section>
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
-          <AccordionItem className="accordion-item" title="We got 20 years experience">
+          <AccordionItem
+            id="experience"
+            className="accordion-item"
+            title="We got 20 years experience"
+          >
             <article>
               <p>You can&apos;t go wrong with us.</p>
-              <p>We are in the business of planning highly individualised  vaction trips for more han 20 years.</p>
+              <p>We are in the business of planning highly individualised vaction trips for more han 20 years.</p>
             </article>
           </AccordionItem>
-          <AccordionItem className="accordion-item" title="We're wokring with local guides">
+          <AccordionItem
+            id="local-guides"
+            className="accordion-item"
+            title="We're working with local guides"
+          >
             <article>
               <p>We are not doing this along from our office.</p>
               <p>Instead we are working with local guides to ensure a safe and pleasant vacation.</p>
             </article>
           </AccordionItem>
         </Accordion>
-      </seciton>
+      </section>
     </main>
   )
 }
--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,6 +1,39 @@
+import { createContext, useState } from "react";
+import { useContext } from "react";
+
+const AccordionContext = createContext();
+
+export function useAccordionContext() {
+  const ctx = useContext(AccordionContext)
+
+  if (!ctx) {
+    throw new Error('Accordion-related component must be wrapped by <Accordion/>');
+  }
+
+  return ctx;
+}
+
 export default function Accordion({ children, className }) {
+  const [openItemId, setOpenItemId] = useState();
+  
+  function openItem(id) {
+    setOpenItemId(id)
+  }
+
+  function closeItem() {
+    setOpenItemId(null)
+  }
+  
+  const contextValue = {
+    openItemId: openItemId,
+    openItem,
+    closeItem
+  }
+
   return (
-    <ul className={className}>{children}</ul>
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>{children}</ul>
+    </AccordionContext.Provider>
   );
 }
 
