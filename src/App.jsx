d8ef7050d2bd35957df44718346b1bb16143bff7
bubsworth <bubbard96@hotmail.com>
2024-04-08 18:02:58 +0100

    Sharing Cross-Component State When Working With Compound Components

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,12 @@ function App() {
     <main>
       <section>
         <h2>Why work with us?</h2>
-        <Accordion className="accordion">
-          <Accordion.Item id="experience" className="accordion-item">
+        <Accordion id="experience" className="accordion">
+          <Accordion.Item className="accordion-item">
             <Accordion.Title className="accordion-item-title" id="experience">
               We got 20 years experience
             </Accordion.Title>
-            <Accordion.Content
-              id="experience"
-              className="accordion-item-content"
-            >
+            <Accordion.Content className="accordion-item-content">
               <article>
                 <p>You can&apos;t go wrong with us.</p>
                 <p>
@@ -23,14 +20,11 @@ function App() {
               </article>
             </Accordion.Content>
           </Accordion.Item>
-          <Accordion.Item className="accordion-item">
-            <Accordion.Title className="accordion-item-title" id="local-guides">
+          <Accordion.Item id="local-guides" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
               We're working with local guides
             </Accordion.Title>
-            <Accordion.Content
-              id="local-guides"
-              className="accordion-item-content"
-            >
+            <Accordion.Content className="accordion-item-content">
               <article>
                 <p>We are not doing this along from our office.</p>
                 <p>
