447180eccdfb428fdb91737a78dafb62e0c159a5
bubsworth <bubbard96@hotmail.com>
2024-04-08 17:51:37 +0100

    Adding Extra Components For Reusability & Configurability

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,39 @@ function App() {
       <section>
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
-          <Accordion.Item
-            id="experience"
-            className="accordion-item"
-            title="We got 20 years experience"
-          >
-            <article>
-              <p>You can&apos;t go wrong with us.</p>
-              <p>
-                We are in the business of planning highly individualised vaction
-                trips for more han 20 years.
-              </p>
-            </article>
+          <Accordion.Item id="experience" className="accordion-item">
+            <Accordion.Title className="accordion-item-title" id="experience">
+              We got 20 years experience
+            </Accordion.Title>
+            <Accordion.Content
+              id="experience"
+              className="accordion-item-content"
+            >
+              <article>
+                <p>You can&apos;t go wrong with us.</p>
+                <p>
+                  We are in the business of planning highly individualised
+                  vaction trips for more han 20 years.
+                </p>
+              </article>
+            </Accordion.Content>
           </Accordion.Item>
-          <Accordion.Item
-            id="local-guides"
-            className="accordion-item"
-            title="We're working with local guides"
-          >
-            <article>
-              <p>We are not doing this along from our office.</p>
-              <p>
-                Instead we are working with local guides to ensure a safe and
-                pleasant vacation.
-              </p>
-            </article>
+          <Accordion.Item className="accordion-item">
+            <Accordion.Title className="accordion-item-title" id="local-guides">
+              We're working with local guides
+            </Accordion.Title>
+            <Accordion.Content
+              id="local-guides"
+              className="accordion-item-content"
+            >
+              <article>
+                <p>We are not doing this along from our office.</p>
+                <p>
+                  Instead we are working with local guides to ensure a safe and
+                  pleasant vacation.
+                </p>
+              </article>
+            </Accordion.Content>
           </Accordion.Item>
         </Accordion>
       </section>
