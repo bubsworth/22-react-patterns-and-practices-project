14353ad348cbde3706972ece3448ee367fa672aa
bubsworth <bubbard96@hotmail.com>
2024-04-08 18:33:05 +0100

    Implementing a Search Functionality With Help Of Render Props

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import amazonImg from "./assets/amazon-river.jpg";
 import caribbeanImg from "./assets/caribbean-beach.jpg";
 import desertImg from "./assets/desert-dunes.jpg";
 import forestImg from "./assets/forest-waterfall.jpg";
+import Place from "./Place";
 
 const PLACES = [
   {
@@ -76,8 +77,12 @@ function App() {
         </Accordion>
       </section>
       <section>
-        <SearchableList items={PLACES} />
-        <SearchableList items={["item 1", "item 2"]} />
+        <SearchableList items={PLACES}>
+          {(item) => <Place item={item} />}
+        </SearchableList>
+        <SearchableList items={["item 1", "item 2"]}>
+          {(item) => item}
+        </SearchableList>
       </section>
     </main>
   );
